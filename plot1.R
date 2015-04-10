1d20adf9f60c9822385cd71879717b813b129506
ASmallLemon <xiaoting0523@126.com>
2015-04-10 20:06:01 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,10 @@
+##read the data file
 datasets<-read.table("household_power_consumption.txt",sep = ";",header = TRUE)
+##extract the needed data from the whole data
 dataset<-subset(datasets,Date=="1/2/2007"|Date=="2/2/2007")
+##create and open a png file
 png(filename = "plot1.png",width = 480,height = 480)
+##draw the needed histogram and set needed labels
 hist(as.numeric(as.character(dataset$Global_active_power)),col="Red",xlab = "Global Active Power(kilowatts)",main = "Global Active Power")
+##close png device
 dev.off()
