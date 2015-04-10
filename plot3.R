29a5f0ae7c9ca823803564746f674d1a9836d7e0
ASmallLemon <xiaoting0523@126.com>
2015-04-10 20:24:05 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,11 +1,16 @@
+##read the data file
 datasets<-read.table("household_power_consumption.txt",sep = ";",header = TRUE)
+##extract the needed data from the whole data
 dataset<-subset(datasets,Date=="1/2/2007"|Date=="2/2/2007")
+##change the Date and Time to regular format
 dataset$Date<-as.Date(dataset$Date,"%d/%m/%Y")
 x<-paste(dataset$Date,dataset$Time)
 x<-strptime(x,"%Y-%m-%d %H:%M:%S")
+##transform all the needed data into numerics
 dataset$Sub_metering_1<-as.numeric(as.character(dataset$Sub_metering_1))
 dataset$Sub_metering_2<-as.numeric(as.character(dataset$Sub_metering_2))
 dataset$Sub_metering_3<-as.numeric(as.character(dataset$Sub_metering_3))
+##clear local settings to avoid mistakes
 Sys.setlocale("LC_ALL", "C")##clear local setting,turn Chinese to English
 png(filename = "plot3.png",width = 480,height = 480)
 plot(x,dataset$Sub_metering_1,type = "l",col="black",xlab="",ylab = "Energy sub metering")
